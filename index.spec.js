8c3fb68162af04a1f571f885a2e7f9c1e9f32b20
Kevin Nguyen <kevinn858@gmail.com>
2018-10-29 14:05:03 -0700

    got server listening and a test working

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,11 @@
 const server = require('./server.js');
 const req = require('supertest');
 
-describe('server', () => {});
+describe('server', () => {
+    it('Should have a server listening.', async () => {
+        const res = await req(server).get('/');
+        expect(res.status).toBe(200);
+        expect(res.type).toBe('application/json');
+        expect(res.body).toEqual({message:'Server is listening.'});
+    });
+});
