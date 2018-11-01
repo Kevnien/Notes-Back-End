2b4219d58c21f3a6d192eb7622d67eb3aa76666e
Kevin Nguyen <kevinn858@gmail.com>
2018-11-01 13:00:38 -0700

    fixed typo for port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('dotenv').config();
 const server = require('./server.js');
-const port = process.enc.PORT || 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`Server is running on port ${port}.`));
